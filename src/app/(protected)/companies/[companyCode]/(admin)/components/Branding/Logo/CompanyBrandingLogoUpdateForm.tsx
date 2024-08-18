10a8ef9206b6691d83141d72e2782cf2477e76c8
hodoje <nikolakaraklic@gmail.com>
2024-08-18 17:28:43 +0200

    move file upload svg component to file uploader
    Signed-off-by: hodoje <nikolakaraklic@gmail.com>

--- src/app/(protected)/companies/[companyCode]/(admin)/components/Branding/Logo/CompanyBrandingLogoUpdateForm.tsx
+++ src/app/(protected)/companies/[companyCode]/(admin)/components/Branding/Logo/CompanyBrandingLogoUpdateForm.tsx
@@ -4,7 +4,7 @@ import { Company, useReadCompany, updateCompanyBrandingLogo } from "@/api/compan
 import { updateCompanyBrandingLogoSchema } from "@/app/(protected)/companies/schemas";
 import { Header } from "@/components";
 import { FileUploader } from "@/components/FileUploader";
-import { FileUploaderContent, FileInput, FileUploaderItem } from "@/components/FileUploader/FileUploader";
+import { FileUploaderContent, FileInput, FileUploaderItem, FileSvgDraw } from "@/components/FileUploader/FileUploader";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -18,35 +18,6 @@ interface CompanyUpdateFormProps {
 }
 
 type CompanyBrandingLogoUpdateFormData = z.infer<typeof updateCompanyBrandingLogoSchema>
-
-const FileSvgDraw = () => {
-    return (
-      <>
-        <svg
-          className="w-8 h-8 mb-3 text-gray-500 dark:text-gray-400"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 16"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-          />
-        </svg>
-        <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-          <span className="font-semibold">Click to upload</span>
-          &nbsp; or drag and drop
-        </p>
-        <p className="text-xs text-gray-500 dark:text-gray-400">
-          PNG, JPG or JPEG
-        </p>
-      </>
-    );
-  };
 
 export const CompanyBrandingLogoUpdateForm = ({ companyCode }: CompanyUpdateFormProps) => {
     const [files, setFiles] = useState<File[] | null>(null);
