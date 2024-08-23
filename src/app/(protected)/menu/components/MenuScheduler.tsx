64b2409759799af9fcf39233874933fe616f3e0a
hodoje <nikolakaraklic@gmail.com>
2024-08-23 19:28:10 +0200

    fix meal assignment form
    Signed-off-by: hodoje <nikolakaraklic@gmail.com>

--- src/app/(protected)/menu/components/MenuScheduler.tsx
+++ src/app/(protected)/menu/components/MenuScheduler.tsx
@@ -58,17 +58,17 @@ export const MealScheduler = () => {
     })
 
     const initialFetch = async () => {
-        setIsLoading(true)
         const mealsResult = await readMeals({ path: '', query: { pagination: { ...DEFAULT_COLLECTION_OFFSET_PAGINATION_REQUEST }}})
         setMeals(mealsResult.results)
         const { firstDay, lastDay } = getFirstAndLastDayOfMonth(currentMonth)
         const dailyMenusResult = await readDailyMenus({ path: '', query: { pagination: {...DEFAULT_COLLECTION_OFFSET_PAGINATION_REQUEST}, filter: {from: firstDay, to: lastDay}}})
         setDailyMenus(dailyMenusResult)
-        setIsLoading(false)
     }
 
     useEffect(() => {
+        setIsLoading(true)
         initialFetch()
+        setIsLoading(false)
     }, [currentMonth])
 
     const toggleMode = () => {
@@ -128,7 +128,6 @@ export const MealScheduler = () => {
             setSelectedDates([])
             setSelectedMeals([])
             setValue([])
-            setIsAssignmentMode(!isAssignmentMode)
             initialFetch()
         })
     }
@@ -139,91 +138,121 @@ export const MealScheduler = () => {
 
     return (
         <Form {...form}>
-            <form onSubmit={handleSubmit(onSubmit)} className='space-y-8 h-full'>
-                <div className="flex justify-between items-center">
-                    <Header heading={isAssignmentMode ? 'Assign Meals to Dates' : 'View Daily Menu'} />
-                    <Button type="button" variant="outline" onClick={toggleMode}>
-                        {isAssignmentMode ? 'Switch to View Mode' : 'Switch to Assign Mode'}
-                    </Button>
-                </div>
+    <form onSubmit={handleSubmit(onSubmit)} className='space-y-8 h-full'>
+        <div className="flex justify-between items-center">
+            <Header heading={isAssignmentMode ? 'Assign Meals to Dates' : 'View Daily Menu'} />
+            <Button type="button" variant="outline" onClick={toggleMode}>
+                {isAssignmentMode ? 'Switch to View Mode' : 'Switch to Assign Mode'}
+            </Button>
+        </div>
 
-                <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
-                    <Calendar
-                        mode={isAssignmentMode ? 'multiple' : 'single'}
-                        selected={selectedDates}
-                        month={currentMonth}
-                        onSelect={handleDateSelection}
-                        onMonthChange={onMonthChange}
-                        className="bg-white shadow-lg rounded-lg"
-                    />
+        <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+            <Calendar
+                mode={isAssignmentMode ? 'multiple' : 'single'}
+                selected={selectedDates}
+                month={currentMonth}
+                onSelect={handleDateSelection}
+                onMonthChange={onMonthChange}
+                className="bg-white shadow-lg rounded-lg"
+            />
 
-                    <div className="flex-grow min-w-[300px] bg-white shadow-lg rounded-lg p-4 flex flex-col justify-between">
-                        <div className="flex-grow">
-                            <h3 className="text-lg font-semibold mb-4">
-                                {isAssignmentMode ? 'Assign Meals to Dates' : `Meals for ${selectedDates.length === 1 ? selectedDates[0].toDateString() : 'Selected Dates'}`}
-                            </h3>
-                            {!isAssignmentMode && selectedMeals.length > 0 && (
-                                <div className="grid gap-4 max-h-100 overflow-y-auto">
-                                    {selectedMeals.map(meal => (
-                                        <div key={meal.id} className="bg-gray-100 p-4 rounded-lg shadow-md flex items-center">
-                                            {meal.imageUrl && (
-                                                <img src={meal.imageUrl} alt={meal.name} className="w-16 h-16 rounded-lg mr-4" />
-                                            )}
-                                            <div>
-                                                <h4 className="text-md font-semibold">{meal.name}</h4>
-                                                <p className="text-sm text-gray-600"><b>Description:</b> {meal.description}</p>
-                                                <p className='text-sm text-gray-600'><b>Type:</b> {meal.type}</p>
-                                                <p className="text-sm text-gray-600">
-                                                    <b>Purchase Price:</b> {meal.purchasePrice.toFixed(2)} RSD | <b>Selling Price:</b> {meal.sellingPrice.toFixed(2)} RSD
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                            {!isAssignmentMode && selectedDates.length > 0 && selectedMeals.length === 0 && (
-                                <p className="text-gray-500">No meals assigned for the selected day</p>
-                            )}
-                            {!isAssignmentMode && selectedDates.length === 0 && (
-                                <div className="flex items-center justify-center h-full">
-                                    <p className="text-gray-500 text-center">Please select a date to view the menu.</p>
-                                </div>
-                            )}
-                            {isAssignmentMode && selectedDates.length > 0 && (
-                                <>
-                                    <FormField
-                                        control={control}
-                                        name='meals'
-                                        render={({ field }) => (
-                                            <MultiSelector
-                                                onValuesChange={handleMealSelection}
-                                                values={value}
-                                                loop
-                                                className='max-w-xs'>
-                                                <MultiSelectorTrigger>
-                                                    <MultiSelectorInput placeholder='Select your meals' />
-                                                </MultiSelectorTrigger>
-                                                <MultiSelectorContent>
-                                                    <MultiSelectorList>
-                                                        {meals?.map(meal => (
-                                                            <MultiSelectorItem key={meal.id} value={meal.id}>
-                                                                {meal.name}
-                                                            </MultiSelectorItem>
-                                                        ))}
-                                                    </MultiSelectorList>
-                                                </MultiSelectorContent>
-                                            </MultiSelector>
+            <div className="flex-grow min-w-[300px] bg-white shadow-lg rounded-lg p-4 flex flex-col flex-start">
+                <h3 className="text-lg font-semibold mb-4">
+                    {isAssignmentMode ? 'Assign Meals to Dates' : `Meals for ${selectedDates.length === 1 ? selectedDates[0].toDateString() : 'Selected Dates'}`}
+                </h3>
+                <div className="flex-grow max-h-[500px] overflow-y-auto">
+                    {!isAssignmentMode && selectedMeals.length > 0 && (
+                        <div className="grid gap-4">
+                            {selectedMeals.map(meal => (
+                                <div key={meal.id} className="bg-gray-100 p-4 rounded-lg shadow-md flex items-center">
+                                    <div className="w-20 h-20 bg-gray-200 rounded-lg mr-4">
+                                        {meal.imageUrl ? (
+                                            <img
+                                                src={meal.imageUrl}
+                                                alt={meal.name}
+                                                className="w-20 h-20 object-cover rounded-lg"
+                                            />
+                                        ) : (
+                                            <div className="w-20 h-20 bg-gray-200 rounded-lg"></div>
                                         )}
-                                    />
-                                </>
-                            )}
+                                    </div>
+                                    <div>
+                                        <h4 className="text-md font-semibold">{meal.name}</h4>
+                                        <p className="text-sm text-gray-600"><b>Description:</b> {meal.description}</p>
+                                        <p className='text-sm text-gray-600'><b>Type:</b> {meal.type}</p>
+                                        <p className="text-sm text-gray-600">
+                                            <b>Purchase Price:</b> {meal.purchasePrice.toFixed(2)} RSD | <b>Selling Price:</b> {meal.sellingPrice.toFixed(2)} RSD
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                        {isAssignmentMode && selectedDates.length > 0 && (
-                            <Button type='submit' className="w-full mt-4" disabled={value.length === 0}>Assign Meals</Button>
-                        )}
-                    </div>
+                    )}
+                    {!isAssignmentMode && selectedDates.length > 0 && selectedMeals.length === 0 && (
+                        <p className="text-gray-500">No meals assigned for the selected day</p>
+                    )}
+                    {!isAssignmentMode && selectedDates.length === 0 && (
+                        <div className="flex items-center justify-center h-full">
+                            <p className="text-gray-500 text-center">Please select a date to view the menu.</p>
+                        </div>
+                    )}
+                    {isAssignmentMode && (
+                        <div className="grid gap-4">
+                            {meals.map(meal => (
+                                <div
+                                    key={meal.id}
+                                    className={`bg-gray-100 p-4 rounded-lg shadow-md flex items-center cursor-pointer ${value.includes(meal.id) ? 'bg-blue-100' : ''}`}
+                                    onClick={() => handleMealSelection(value.includes(meal.id) ? value.filter(id => id !== meal.id) : [...value, meal.id])}
+                                >
+                                    <div className="w-16 h-16 bg-gray-200 rounded-lg mr-4">
+                                        {meal.imageUrl ? (
+                                            <img src={meal.imageUrl} alt={meal.name} className="w-full h-full object-cover rounded-lg" />
+                                        ) : (
+                                            <div className="w-full h-full bg-gray-200 rounded-lg"></div>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <h4 className="text-md font-semibold">{meal.name}</h4>
+                                        <p className="text-sm text-gray-600"><b>Description:</b> {meal.description}</p>
+                                        <p className='text-sm text-gray-600'><b>Type:</b> {meal.type}</p>
+                                        <p className="text-sm text-gray-600">
+                                            <b>Purchase Price:</b> {meal.purchasePrice.toFixed(2)} RSD | <b>Selling Price:</b> {meal.sellingPrice.toFixed(2)} RSD
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
-            </form>
-        </Form>
+                {isAssignmentMode && (
+                    <>
+                        {value.length === 0 && selectedDates.length === 0 && (
+                            <Button type='submit' className="w-full mt-4" disabled>
+                                Please select a date and at least one meal
+                            </Button>
+                        )}
+                        {value.length === 0 && selectedDates.length > 0 && (
+                            <Button type='submit' className="w-full mt-4" disabled>
+                                Please select at least one meal
+                            </Button>
+                        )}
+                        {value.length > 0 && selectedDates.length === 0 && (
+                            <Button type='submit' className="w-full mt-4" disabled>
+                                Please select a date
+                            </Button>
+                        )}
+                        {value.length > 0 && selectedDates.length > 0 && (
+                            <Button type='submit' className="w-full mt-4">
+                                Assign Meals
+                            </Button>
+                        )}
+                    </>
+                )}
+            </div>
+        </div>
+    </form>
+</Form>
+
+
     )
 }
