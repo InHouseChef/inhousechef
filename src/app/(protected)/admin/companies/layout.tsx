eda78d83e3e9547b447cdad226b08c0002f36f8c
hodoje <nikolakaraklic@gmail.com>
2024-08-24 23:11:27 +0200

    fix single company navigation
    Signed-off-by: hodoje <nikolakaraklic@gmail.com>

--- src/app/(protected)/admin/companies/layout.tsx
+++ src/app/(protected)/admin/companies/layout.tsx
@@ -1,16 +1,22 @@
 'use client'
 
-import { TopNav } from '@/components'
+import { MainTopNav } from '@/components/MainNav'
 import { ReactNode } from 'react'
+import { COMPANY_LINKS } from './[companyCode]/constants'
+import { usePathParams } from '@/hooks'
 
 interface LayoutProps {
     children: ReactNode
 }
 
 export default function Layout({ children }: LayoutProps) {
+    const { companyCode } = usePathParams<{ companyCode: string }>()
+
+    const links = COMPANY_LINKS.map(({ path, label }) => ({ path: `/admin/companies/${companyCode}${path}`, label }))
+
     return (
         <>
-            <TopNav />
+            <MainTopNav links={links}/>
             {children}
         </>
     )
