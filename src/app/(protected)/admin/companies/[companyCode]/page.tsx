aa48451a13169c04973575f3460e65fe6719e3c2
hodoje <nikolakaraklic@gmail.com>
2024-08-25 03:54:27 +0200

    fix admin company layout
    Signed-off-by: hodoje <nikolakaraklic@gmail.com>

--- src/app/(protected)/admin/companies/[companyCode]/page.tsx
+++ src/app/(protected)/admin/companies/[companyCode]/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import { MainTopNav } from '@/components/MainNav'
 import { CompanyBrandingColorsUpdateForm } from './components/Branding/Colors/CompanyBrandingColorsUpdateForm'
 import { CompanyBrandingLogoUpdateForm } from './components/Branding/Logo/CompanyBrandingLogoUpdateForm'
 import { DangerZone } from './components/DangerZone/DangerZoneForm'
 import { CompanyDetailsUpdateForm } from './components/Details/CompanyDetailsUpdateForm'
-import { COMPANY_LINKS } from './constants'
 
 export default function CompanyPage({ params }: { params: { companyCode: string } }) {
     const { companyCode } = params
-    const links = COMPANY_LINKS.map(({ path, label }) => ({ path: `/admin/companies/${companyCode}${path}`, label }))
 
     return (
         <>
-            <MainTopNav links={links}/>
-
             <div className='border-b border-gray-300 pb-4'>
                 <CompanyDetailsUpdateForm companyCode={companyCode} />
             </div>
