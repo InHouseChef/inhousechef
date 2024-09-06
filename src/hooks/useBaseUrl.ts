d3e82c7dfa5febe41b64cf1499e5d423baccae54
hodoje <nikolakaraklic@gmail.com>
2024-09-06 23:22:18 +0200

    try fix base url
    Signed-off-by: hodoje <nikolakaraklic@gmail.com>

--- src/hooks/useBaseUrl.ts
+++ src/hooks/useBaseUrl.ts
@@ -3,7 +3,7 @@
 export const useBaseUrl = () => {
     const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`
     const adminUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/admin`
-    const employeeUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/employee`
+    const employeeUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`
 
     return { baseUrl, adminUrl, employeeUrl }
 }
